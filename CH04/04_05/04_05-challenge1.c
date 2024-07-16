00941b199ac91261612e4287de0462c234a06eb3
ragatep <ragatep@gmail.com>
2024-07-15 22:28:29 -0700

    Stopped at Chapter 04's first code challenge.

--- CH04/04_05/04_05-challenge1.c
+++ CH04/04_05/04_05-challenge1.c
@@ -7,6 +7,7 @@ int main()
 	printf("Select item 1, 2, or 3: ");
 	scanf("%d",&a);
 
+	/*
 	if( a == 1 )
 		puts("This is the first item");
 	else if( a == 2 )
@@ -15,6 +16,22 @@ int main()
 		puts("This is the third item");
 	else
 		puts("Invalid choice!");
+	*/
+
+	switch(a)
+	{
+		case 1:
+			puts("This is the first item");
+			break;
+		case 2:
+			puts("This is the second item");
+			break;
+		case 3:
+			puts("This is the third item");
+			break;
+		default:
+			puts("Invalid choice!");
+	}
 
 	return(0);
 }
