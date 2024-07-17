f40aba86b6f905b2c56359bb050ccbf790682d84
ragatep <ragatep@gmail.com>
2024-07-16 23:09:21 -0700

    Chapter 4 completed.

--- CH04/04_09/04_09-challenge1.c
+++ CH04/04_09/04_09-challenge1.c
@@ -7,7 +7,32 @@ int main()
 	printf("How long is the line? ");
 	scanf("%d",&a);
 
-	/* write the loop here */
+	/* 
+	write a for loop that draws a line of hyphen characters.
+	User input sets the line length
+	The loop outputs a single hyphen character based on the user input.
+	Use the putchar() function to output the hyphen character.
+	*/
 
-	return(0);
+	/*
+	// Solution 1 - using a for loop
+		for(int i = 0; i < a; i++)
+	{
+		putchar('-');
+	}
+
+	putchar('\n');
+
+	*/
+// Solution 2 - using a while loop
+	int i = 0;
+	while(i < a)
+	{
+		putchar('-');
+		i++;
+	}
+	putchar('\n');
+
+return(0);
+
 }
--- CH04/04_14/04_14-challenge1.c
+++ CH04/04_14/04_14-challenge1.c
@@ -7,19 +7,48 @@
 int main()
 {
 	// variable declarations
+	int done;
+	char input;
 			
 	/* main program loop */
 	done = FALSE;
 	while(!done)
 	{
-		// prompt for input
-		// process input
+		// prompt for input 
+		printf("Enter a command (H for help): ");
+		// process input. Use the getchar() function to read a character from the keyboard
 		// if R is input, output "move right"
 		// if L is input, output "move left"
 		// if B is input, output "move back"
 		// if Q is input, break the loop
 		// if H or ? is input, output valid commands
 		// identify invalid input
+		input = getchar();
+		switch(input)
+		{
+			case 'R':
+				printf("move right\n");
+				break;
+			case 'L':
+				printf("move left\n");
+				break;
+			case 'B':
+				printf("move back\n");
+				break;
+			case 'Q':
+				done = TRUE;
+				break;
+			case 'H':
+			case '?':
+				printf("Valid commands are R, L, B, Q, H, ?\n");
+				break;
+			default:
+				printf("Invalid command\n");
+				break;
+		}
+		// Flushing the input buffer
+		while(getchar() != '\n');
+
 	}
 
 	return 0;
