71de8617cfc9ccee5376ad5e7ada0b2b24364d37
ragatep <ragatep@gmail.com>
2024-07-25 19:57:14 -0700

    Completed first challenge.

--- CH08/08_05/08_05-challenge1.c
+++ CH08/08_05/08_05-challenge1.c
@@ -2,7 +2,53 @@
 
 int main()
 {
+    char title[] = "Pointers don't intimidate me!\n";
+    char *ptr = title; // Assign a pointer to the string
+	int x = 0;
+
+    // Use the pointer to output each character in the string
+    while (*(ptr+x) != '\0') {
+        putchar(*(ptr+x));
+        x++; // Move the pointer to the next character
+    }
+
+    return 0;
+}
+
+
+
+/*	
+Solution no. 1:
+
+#include <stdio.h>
+
+int main()
+{
+    char title[] = "Pointers don't intimidate me!\n";
+    char *ptr = title; // Assign a pointer to the string
+
+    // Use the pointer to output each character in the string
+    while (*ptr != '\0') {
+        putchar(*ptr);
+        ptr++; // Move the pointer to the next character
+    }
+
+    return 0;
+}
+*/
+
+
+/*
+
+Skeleton code for the challenge:
+
+#include <stdio.h>
+
+int main()
+{
 	char title[] = "Pointers don't intimidate me!\n";
 
 	return 0;
 }
+
+*/
